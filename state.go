68dbc2a38679c39534d8e0ffd9606051a1c9d72e
MichalMitros <mitrosmicha4@gmail.com>
2026-08-11 21:17:45 +0200

    refactor: remove unused IsKeyPressed function from State

--- state.go
+++ state.go
@@ -4,7 +4,6 @@ import (
 	"image/color"
 
 	"github.com/MichalMitros/sketch/vector"
-	"github.com/hajimehoshi/ebiten/v2"
 )
 
 // State is the state of the sketch used to provide information about the current state of the sketch to Update().
@@ -25,9 +24,4 @@ func newState(width, height int, backgroundColor color.Color) *State {
 // ScreenSize returns the width and height of the screen.
 func (s *State) ScreenSize() vector.Vector {
 	return vector.New(float64(s.width), float64(s.height))
-}
-
-// IsKeyPressed returns true if the given key is currently pressed.
-func (s *State) IsKeyPressed(k Key) bool {
-	return ebiten.IsKeyPressed(ebiten.Key(k))
 }
